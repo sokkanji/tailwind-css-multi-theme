5477cfec9dee6cdafc828f54df50e0d3bf5d1e98
Minji So <sskkanji@gmail.com>
2024-01-08 16:56:18 +0900

    Refactoring CheckList, Content

--- app/components/CheckLIst.tsx
+++ app/components/CheckLIst.tsx
@@ -1,44 +1,35 @@
 export default function CheckList() {
+  const textList = [
+    <p className="ml-4">
+      <code className="text-sm font-bold text-gray-900">global.css</code>에 스타일 변수 선언하기
+    </p>,
+    <p className="ml-4">
+      <code className="text-sm font-bold text-gray-900">tailwind.config.ts</code>에 스타일 변수
+      선언하기
+    </p>,
+    <p className="ml-4">스타일 변수로 컴포넌트에 스타일 작성하기</p>,
+    <p className="ml-4">
+      <code className="text-sm font-bold text-gray-900">localStorage</code>를 이용해서 테마 저장하기
+    </p>,
+  ];
+
   return (
     <ul className="space-y-4">
-      <li className="flex items-center">
-        <svg
-          className="h-6 w-6 flex-none fill-secondary stroke-primary stroke-2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <circle cx="12" cy="12" r="11" />
-          <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-        </svg>
-        <p className="ml-4">
-          Customizing your&nbsp;
-          <code className="text-sm font-bold text-gray-900">tailwind.config.js</code> file
-        </p>
-      </li>
-      <li className="flex items-center">
-        <svg
-          className="h-6 w-6 flex-none fill-secondary stroke-primary stroke-2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <circle cx="12" cy="12" r="11" />
-          <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-        </svg>
-        <p className="ml-4">
-          Extracting classes with <code className="text-sm font-bold text-gray-900">@apply</code>
-        </p>
-      </li>
-      <li className="flex items-center">
-        <svg
-          className="h-6 w-6 flex-none fill-secondary stroke-primary stroke-2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <circle cx="12" cy="12" r="11" />
-          <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-        </svg>
-        <p className="ml-4">Code completion with instant preview</p>
-      </li>
+      {textList.map((item, i) => {
+        return (
+          <li key={i} className="flex items-center">
+            <svg
+              className="h-6 w-6 flex-none fill-secondary stroke-primary stroke-2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="12" cy="12" r="11" />
+              <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+            </svg>
+            {item}
+          </li>
+        );
+      })}
     </ul>
   );
 }
--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -4,13 +4,13 @@ export default function Content() {
   return (
     <div className="divide-y divide-gray-300/50">
       <div className="space-y-6 py-4 text-base leading-7 text-gray-600">
-        <h2 className="text-3xl text-primary">3가지 커스텀 테마 구현하기</h2>
+        <h2 className="text-3xl text-primary">커스텀 테마 3가지 이상 구현하기</h2>
 
         <CheckList />
 
         <p>
-          아래 버튼을 클릭하여 테마 색상을 변경해 보세요. <br />
-          테마가 변경할 때마다 로컬 스토리지에 지정한 테마가 저장됩니다. <br />
+          아래 버튼을 클릭하여 테마를 변경해 보세요. <br />
+          테마가 변경될 때마다 로컬 스토리지에 지정한 테마가 저장됩니다. <br />
           새로고침을 하거나 창을 닫고 재접속을 해도 <br />
           제일 최근에 선택한 테마로 유지됩니다 :)
         </p>
